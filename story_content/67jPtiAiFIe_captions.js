4d6496baefc02dc5d7e9eb8208ada19f275d448c
sp-sen <ashiqien_shah@ichat.sp.edu.sg>
2021-10-09 01:41:02 +0800

    unit-4
    unit-4

---
+++ story_content/67jPtiAiFIe_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.55.25975.0%0D%0A%0D%0A00:00:00.056%20--%3E%2000:00:03.214%0D%0AClick%20on%20the%20arrow%20to%20read%20on%20the%20next%20emotion.%0D%0A%0D%0A"}')
---
+++ story_content/6EuoovaYeRL_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.55.25975.0%0D%0A%0D%0A00:00:00.064%20--%3E%2000:00:01.696%0D%0APlease%20read%20for%20Demanding%20customers.%0D%0A%0D%0A"}')
